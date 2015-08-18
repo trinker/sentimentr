3cff8598bfbb521ce4d37b73e830555430dea6f8
trinker <tyler.rinker@gmail.com>
2015-08-18 11:24:27 -0400

    added to polarity dictionary and made script for ease of changing dictionary

--- R/sentimentr-package.R
+++ R/sentimentr-package.R
@@ -22,7 +22,7 @@ NULL
 #' @keywords datasets
 #' @name polarity_table
 #' @usage data(polarity_table)
-#' @format A data frame with 6776 rows and 2 variables
+#' @format A data frame with 6780 rows and 2 variables
 #' @references Hu, M., & Liu, B. (2004). Mining opinion features in customer
 #' reviews. National Conference on Artificial Intelligence.
 #'
@@ -63,7 +63,7 @@ NULL
 #' @keywords datasets
 #' @name valence_shifters_table
 #' @usage data(valence_shifters_table)
-#' @format A data frame with 85 rows and 2 variables
+#' @format A data frame with 86 rows and 2 variables
 NULL
 
 
---
+++ inst/add_to_dictionary.R
@@ -0,0 +1,59 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr, data.table)
+pacman::p_load_gh("trinker/sentimentr", "trinker/pax")
+
+#====================================================
+#polarity table
+
+# check if word is already there
+polarity_table["helped", ]
+
+polarity_table <- sentimentr::polarity_table %>%
+	   as.data.frame()
+
+(check <- nrow(polarity_table))
+
+
+polarity_table[check + 1, ] <- NA
+polarity_table[check + 1, "x"] <- "helped"
+polarity_table[check + 1, "y"] <- 1
+
+
+polarity_table <- polarity_table %>%
+	   arrange(x)
+
+nrow(polarity_table)
+nrow(polarity_table) == check + 1
+
+data.table::setDT(polarity_table)
+data.table::setkey(polarity_table, "x")
+
+pax::new_data(polarity_table)
+
+# Removing term
+polarity_table <- polarity_table[!1:nrow(polarity_table) %in% tail(which(polarity_table$x == "helped"), -1), ] 
+nrow(polarity_table)
+rownames(polarity_table) <- NULL
+
+#====================================================
+#valence_shifters_table
+valence_shifters_table <- sentimentr::valence_shifters_table %>%
+	   as.data.frame()
+
+(check <- nrow(valence_shifters_table))
+valence_shifters_table[check + 1, ] <- NA
+valence_shifters_table[check + 1, "x"] <- "cares"
+valence_shifters_table[check + 1, "y"] <- 1
+
+
+valence_shifters_table <- valence_shifters_table %>%
+	   arrange(x)
+
+nrow(valence_shifters_table) == check + 1
+
+data.table::setDT(valence_shifters_table)
+data.table::setkey(valence_shifters_table, "x")
+
+pax::new_data(valence_shifters_table)
+
+
