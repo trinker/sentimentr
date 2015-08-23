d6ddbec41d7924118221a0665b67ac64834b8214
Tyler <tyler.rinker@gmail.com>
2015-08-23 14:48:21 -0400

    fixed test to not throw travis-ci error

--- tests/testthat/test-sentiment.R
+++ tests/testthat/test-sentiment.R
@@ -17,6 +17,11 @@ test_that("sentiment produces a data.table with numeric sentiment column",{
 
 test_that("sentiment question.weight = 0 alters the output",{
 
+    mytext <- c(
+       'do you like it?  But I hate really bad dogs',
+       'I am the best friend.',
+       'Do you really like it?  I\'m not a fan'
+    )
     x <- sentiment(mytext)[["sentiment"]]
     y <- sentiment(mytext, question.weight = 0)[["sentiment"]]
 
@@ -25,7 +30,7 @@ test_that("sentiment question.weight = 0 alters the output",{
 
 
 
-test_that("sentiment question.weight = 0 alters the output",{
+test_that("sentiment n.before/n.after alters the output",{
 
     mytext <- c(
        'do you like it?  But I hate really bad dogs',
@@ -38,7 +43,7 @@ test_that("sentiment question.weight = 0 alters the output",{
     expect_false(all(x == y))
 })
 
-test_that("sentiment question.weight = 0 alters the output",{
+test_that("sentiment but.weight = 0 alters the output",{
 
     mytext <- c(
        'do you like it?  But I hate really bad dogs',
