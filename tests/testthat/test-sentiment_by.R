4236e53392779c958cbeae479716bfbbb369f6bf
Tyler <tyler.rinker@gmail.com>
2015-08-23 21:29:30 -0400

    added test for sentiment_by

--- tests/testthat/test-sentiment_by.R
+++ tests/testthat/test-sentiment_by.R
@@ -1,7 +1,20 @@
 context("Checking sentiment_by")
 
-test_that("sentiment_by ...",{
+test_that("sentiment_by gives combined elements when `by = NULL`",{
 
+    mytext <- c(
+       'do you like it?  But I hate really bad dogs',
+       'I am the best friend.',
+       'Do you really like it?  I\'m not happy'
+    )
+
+    expect_true(is(sentiment_by(mytext), "data.table"))
+    expect_true(nrow(sentiment_by(mytext)) == 3)
 
 })
 
+test_that("sentiment_by gives combined group when `by` is given",{
+
+    x <- with(presidential_debates_2012, sentiment_by(dialogue, list(person, time)))
+    expect_true(all(c('person', 'time') %in%colnames(x)))
+})
