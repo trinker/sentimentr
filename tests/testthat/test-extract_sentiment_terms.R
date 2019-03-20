bb66da24aeda995988b0084143ed08b9da0c3efb
Tyler Rinker <trinker@campuslabs.com>
2019-03-19 22:08:47 -0400

    Fixed cran check issues related to set.seed changing API in R dev

--- tests/testthat/test-extract_sentiment_terms.R
+++ tests/testthat/test-extract_sentiment_terms.R
@@ -2,6 +2,7 @@ context("Checking extract_sentiment_terms")
 
 test_that("extract_sentiment_terms extracts the terms",{
 
+    suppressWarnings(RNGversion("3.5.0"))
     set.seed(10)
     x <- get_sentences(sample(hu_liu_cannon_reviews[[2]], 3000, TRUE))
     pol_words <- extract_sentiment_terms(x)
--- tests/testthat/test-update_key.R
+++ tests/testthat/test-update_key.R
@@ -11,8 +11,12 @@ test_that("update_key ...",{
 
     mykey <- as_key(key)
 
-    out <- update_key(mykey, drop = c("f", "h"),
-        x = data.frame(x = c("dog", "cat", "rock"), y = c(1, -1, 1), stringsAsFactors = FALSE))
+    out <- update_key(
+        mykey, 
+        drop = c("f", "h"),
+        x = data.frame(x = c("dog", "cat", "rock"), 
+        y = c(1, -1, 1), stringsAsFactors = FALSE)
+    )
 
     expect_true(is_key(out))
     expect_equal(nrow(out), 27)
